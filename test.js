1bc9b43429345329fa865073138363d0c89904fd
DinaGold <sinmeliodas1999@gmail.com>
2024-07-03 10:14:31 +0500

    Работы с Bind и group

--- test.js
+++ test.js
@@ -9,15 +9,19 @@ const person = {
     age: 0,
     job: '',
     nation: '',
+    phone: '',
     aboutMe() {
         console.log(`Hello, my name is ${this.name}!, I am ${this.job}`);
     },
     info() {
-        console.log(`My name is ${this.name}`);
-        console.log(`I am ${this.age} years old`);
-        console.log(`I am a ${this.job}`);
-        console.log(`I'm from ${this.nation}`);
-        console.log(`I live in ${this.residence}`);
+        console.group(`Информация о ${this.name} :`)
+        console.log(`My name is ${this.name}`)
+        console.log(`I am ${this.age} years old`)
+        console.log(`I am a ${this.job}`)
+        console.log(`I'm from ${this.nation}`)
+        console.log(`I live in ${this.residence}`)
+        console.log(`My phone number is ${this.phone}`)
+        console.groupEnd()
     }
 
 }
@@ -30,6 +34,7 @@ Diana.age = 23
 Diana.nation = 'Korean'
 Diana.job = 'Business lady'
 Diana.residence = 'Unknown'
+Diana.phone = '000 000-00-01'
 
 // Добавление нового Объекта, персонажа со свойствами
 const Adam = Object.create(person)
@@ -38,6 +43,7 @@ Adam.age = 25,
 Adam.nation = 'Unknown',
 Adam.job = 'Vacationist',
 Adam.residence = 'Unknown'
+Adam.phone = '000 000-00-05'
 
 // Другой метод добавление нового Объекта, персонажа со свойствами
 const Zlata = {
@@ -46,6 +52,8 @@ const Zlata = {
     nation: 'Russian',
     job: 'Baby',
     residence: 'Unknown',
+    phone: '000 000-00-02',
+    residence: "Unknown"
 }
 
 // Добавление нового свойства
@@ -55,11 +63,14 @@ Object.prototype.hello = function() {
 // Добавление нового свойства
 Object.prototype.residence = ""
 
+// Привязка свойства
+const FnZlataBindInfo = person.info.bind(Zlata)()
+
 
 // Методы вызова
 console.log(Diana.info());
+console.log(FnZlataBindInfo);
 console.log(Adam.info());
-console.log(person.info.bind(Zlata)());
 // console.log(Diana);
 // console.log(Diana.aboutMe());
 // console.log(Diana.hello());
