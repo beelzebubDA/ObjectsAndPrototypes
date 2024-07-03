25f7c698289c4249343118f2a4dc5f28d52c8305
DinaGold <sinmeliodas1999@gmail.com>
2024-07-03 10:36:43 +0500

    Работа с bind, call, apply

--- test.js
+++ test.js
@@ -36,7 +36,6 @@ Diana.job = 'Business lady'
 Diana.residence = 'Unknown'
 Diana.phone = '000 000-00-01'
 
-// Добавление нового Объекта, персонажа со свойствами
 const Adam = Object.create(person)
 Adam.name = 'Adam',
 Adam.age = 25,
@@ -45,7 +44,6 @@ Adam.job = 'Vacationist',
 Adam.residence = 'Unknown'
 Adam.phone = '000 000-00-05'
 
-// Другой метод добавление нового Объекта, персонажа со свойствами
 const Zlata = {
     name: 'Zlata',
     age: 5,
@@ -53,27 +51,49 @@ const Zlata = {
     job: 'Baby',
     residence: 'Unknown',
     phone: '000 000-00-02',
+    residence: 'Unknown'
+}
+
+const Lilith = {
+    name: 'Lilith',
+    age: 'Unknown',
+    nation: 'Unknown',
+    job: 'Unknown',
+    residence: 'Unknown',
+    phone: '000 000-00-06',
     residence: "Unknown"
+}
+
+const Eva = {
+    name: 'Eva',
+    age: 'Unknown',
+    nation: 'Unknown',
+    job: 'Unknown',
+    residence: 'Unknown',
+    phone: '000 000-00-09',
+    residence: 'Unknown'
 }
 
 // Добавление нового свойства
 Object.prototype.hello = function() {
     console.log(`Hello, ${this.name}!`);
 }
-// Добавление нового свойства
+
 Object.prototype.residence = ""
+
 
 // Привязка свойства
 const FnZlataBindInfo = person.info.bind(Zlata)()
+const FnLilithCallInfo = person.info.call(Lilith)
+const FnLilithApplyInfo = person.info.apply(Eva)
 
 
 // Методы вызова
 console.log(Diana.info());
 console.log(FnZlataBindInfo);
 console.log(Adam.info());
+console.log(FnLilithCallInfo);
+console.log(FnLilithApplyInfo);
 // console.log(Diana);
 // console.log(Diana.aboutMe());
 // console.log(Diana.hello());
-// console.log(Adam);
-// console.log(Adam.aboutMe());
-// console.log(Adam.hello());
